6cb5942492ae684d615f06bd6a4880b3ea6a2e02
Daniel Lemire <github@danlemire.com>
2017-04-28 20:32:38 -0500

    Cleaning up
    Cleaning out unused code. Fixing formatting.

--- parking_assistv2.ino
+++ parking_assistv2.ino
@@ -1,18 +1,11 @@
-/*
- * Using UNOR3, with CharlieWing.
- * 
- * Will need to provide external 5V to Sonar to us 3v3 board, or use a down-stepper.
- * 
- */
+/*************************************************************************************
+ PARTS: UNO, Adafruit CharlieWing, Ultrasonic Sensor (HRC-S04), PIR motion
 
-
-
-
-
-
-
-
-
+ GOAL: when motion is detected, measure the distance detected. Provide the distance to 
+ target on the CharlieWing to provide parking distance estimation useful when backing
+ into the garage.
+   
+*************************************************************************************/
 
 #include <NewPing.h>
 #include <Wire.h>
@@ -20,9 +13,6 @@
 #include <Adafruit_IS31FL3731.h>
 #include "Adafruit_SleepyDog.h"
 
-// If you're using the full breakout...
-//Adafruit_IS31FL3731 matrix = Adafruit_IS31FL3731();
-// If you're using the FeatherWing version
 Adafruit_IS31FL3731_Wing matrix = Adafruit_IS31FL3731_Wing();
 
 #define TRIGGER_PIN  12  // Arduino pin tied to trigger pin on the ultrasonic sensor.
@@ -34,95 +24,49 @@ Adafruit_IS31FL3731_Wing matrix = Adafruit_IS31FL3731_Wing();
 #define PIRgnd       4
 #define PIRaction    13
 
-
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
-unsigned int distanceNow, distanceOld, timeCount = 100;
-bool sleep=false;
+unsigned int distanceNow;
 int PIRread;
 
-void setup() {
+void setup() 
+{
   pinMode(PIR5v, OUTPUT);
   pinMode(PIRgnd,OUTPUT);
   pinMode(PIRtrigger,INPUT);
   digitalWrite(PIR5v,HIGH);
   digitalWrite(PIRgnd,LOW);
-   
-    Serial.begin(9600);
+  Serial.begin(9600);
   Serial.println("ISSI manual animation test");
-  if (! matrix.begin()) {
+  if (! matrix.begin()) 
+  {
     Serial.println("IS31 not found");
     while (1);
   }
   Serial.println("IS31 Found!"); 
-  
 }
 
-void loop() {
-  //matrix.drawFastHLine(1,0,9,255);
-  //matrix.drawFastHLine(6,0,9,255);
+void loop() 
+{
   PIRread = digitalRead(PIRtrigger);
   Serial.println(PIRread);
-  if (PIRread == LOW) { 
-      digitalWrite(PIRaction, HIGH);
-      //Serial.println("Motion Detected!");
-  }
-  else {
-      digitalWrite(PIRaction, LOW);
-      //Serial.println("no motion");
-  }
+  if (PIRread == LOW) digitalWrite(PIRaction, HIGH);
+  else digitalWrite(PIRaction, LOW);
   
-  //delay(30);  // Wait 500ms between pings (about 2 pings/sec). 29ms should be the shortest delay between pings.
   unsigned int uS = sonar.ping(); // Send ping, get ping time in microseconds (uS).
-  //Serial.print("Ping: ");
-  //Serial.print(uS / US_ROUNDTRIP_CM); // Convert ping time to distance and print result (0 = outside set distance range, no ping echo)
-  //Serial.println("cm");
   distanceNow = (uS / US_ROUNDTRIP_CM);
-
-
- if (PIRread == HIGH)
- {
-    //timeCount++;
-    lights(distanceNow);
-     //if (timeCount % 55 == 0)
-    //{
-      //Serial.println("running 10 second loop");
-      //timeCount = 0;
-      //checkSleep();
-      distanceOld = distanceNow;
-    //}
-  }
-  else
-  {
-    matrix.clear();
-    }
-
- 
- 
+  Serial.println(distanceNow);
+  if (PIRread == HIGH) lights(distanceNow);
+  else matrix.clear();
 }
-
-void checkSleep()
-{
-   if (distanceOld == distanceNow || distanceNow == 0)
-    {
-      Serial.println("sleeping 5");
-      matrix.clear();
-      Watchdog.sleep(5000);
-      sleep = true;
-    }
-    else sleep = false;
-}
-
 
 void lights(int distance)
 {
   matrix.setTextSize(1);
-  matrix.setTextWrap(false);  // we dont want text to wrap so it scrolls nicely
+  matrix.setTextWrap(false);
   matrix.setTextColor(100);
- // for (int8_t x=0; x<=32; x++) {
-    matrix.clear();
-    matrix.setCursor(0,0);
-    matrix.print(distance);
-    delay(30);
-  //}
+  matrix.clear();
+  matrix.setCursor(0,0);
+  matrix.print(distance);
+  delay(30);
 }
 
